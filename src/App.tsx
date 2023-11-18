9e0c315c1876993b36990fed6606296174f20023
chenyuhao <cccchenyuhao@gmail.com>
2023-11-18 20:39:45 +0800

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
