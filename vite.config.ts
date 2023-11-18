7ace39bc11a2efc6c29c033ebddf90f2de05c8e5
chenyuhao <cccchenyuhao@gmail.com>
2023-11-18 20:05:13 +0800

    move to vite lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "lib/index.ts"),
-      // the proper extensions will be added
       fileName: "index",
       formats: ["es", "cjs"],
     },
