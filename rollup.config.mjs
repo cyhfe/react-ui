b8a0d12080dbaa69370222b131d96fc5e951858b
chenyuhao <cccchenyuhao@gmail.com>
2023-11-18 16:28:09 +0800

    init ts rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts";
+import pkg from "./package.json" assert { type: "json" };
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "src/index.ts",
+    external: ["react"],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(), // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "esm", sourcemap: true },
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
+];
