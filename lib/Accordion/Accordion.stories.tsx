e4a371417530eb50e723183a912ca5299868929f
chenyuhao <cccchenyuhao@gmail.com>
2023-11-26 12:26:11 +0800

    accordion story

--- lib/Accordion/Accordion.stories.tsx
+++ lib/Accordion/Accordion.stories.tsx
@@ -1,14 +1,38 @@
+export default { title: "Components/Accordion" };
+
 import {
   AccordionContent,
   AccordionItem,
   AccordionRoot,
   AccordionTrigger,
 } from "../";
-export default { title: "Components/Accordion" };
 
 export function Single() {
   return (
     <AccordionRoot type="single">
+      <AccordionItem value="one">
+        <AccordionTrigger className="border">One</AccordionTrigger>
+        <AccordionContent>
+          Per erat orci nostra luctus sociosqu mus risus penatibus, duis elit
+          vulputate viverra integer ullamcorper congue curabitur sociis, nisi
+          malesuada scelerisque quam suscipit habitant sed
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="two">
+        <AccordionTrigger>Two</AccordionTrigger>
+        <AccordionContent>
+          Per erat orci nostra luctus sociosqu mus risus penatibus, duis elit
+          vulputate viverra integer ullamcorper congue curabitur sociis, nisi
+          malesuada scelerisque quam suscipit habitant sed
+        </AccordionContent>
+      </AccordionItem>
+    </AccordionRoot>
+  );
+}
+
+export function SingleCollasible() {
+  return (
+    <AccordionRoot type="single" collapsible>
       <AccordionItem value="one">
         <AccordionTrigger>One</AccordionTrigger>
         <AccordionContent>
