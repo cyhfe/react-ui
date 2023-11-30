381ce5bce9573ea83598c6500175c6991c8b0039
chenyuhao <cccchenyuhao@gmail.com>
2023-11-30 18:39:26 +0800

    select

--- lib/Select/Select.stories.tsx
+++ lib/Select/Select.stories.tsx
@@ -1,17 +1,73 @@
-import { Select, Option } from ".";
+import { SelectOption, SelectRoot, SelectTrigger } from ".";
+import { PopupContent, PopupRoot, PopupTrigger } from "../Popup";
 
 export default {
   title: "Components/Select",
 };
 
-export function SelectDemo() {
+const options = [
+  { value: "ocean", label: "Ocean", color: "#00B8D9" },
+  { value: "blue", label: "Blue", color: "#0052CC" },
+  { value: "purple", label: "Purple", color: "#5243AA" },
+  { value: "red", label: "Red", color: "#FF5630" },
+  { value: "orange", label: "Orange", color: "#FF8B00" },
+  { value: "yellow", label: "Yellow", color: "#FFC400" },
+  { value: "green", label: "Green", color: "#36B37E" },
+  { value: "forest", label: "Forest", color: "#00875A" },
+  { value: "slate", label: "Slate", color: "#253858" },
+  { value: "silver", label: "Silver", color: "#666666" },
+];
+
+export function Single() {
   return (
-    <Select>
-      <Option value="1">one</Option>
-      <Option value="2">two</Option>
-      <Option value="3">three</Option>
-      <Option value="4">four</Option>
-      <Option value="5">five</Option>
-    </Select>
+    <SelectRoot
+      onSelectedChange={(v) => console.log(v)}
+      placeholder="choose color"
+      multiple
+    >
+      <PopupRoot>
+        <PopupTrigger asChild>
+          <SelectTrigger className="w-80 min-h-[30px] px-3 py-2 text-left border rounded shadow" />
+        </PopupTrigger>
+        <PopupContent
+          keepMounted
+          className="flex flex-col gap-y-1 p-2 w-80 min-h-[30px] max-h-[200px] overflow-auto border rounded shadow "
+        >
+          {options.map((option) => {
+            return (
+              <SelectOption
+                value={option.value}
+                className="text-left hover:bg-slate-100 px-3 py-2 data-[selected=true]:bg-blue-100 data-[selected=true]:text-blue-900 rounded  "
+              >
+                {option.label}
+              </SelectOption>
+            );
+          })}
+        </PopupContent>
+      </PopupRoot>
+    </SelectRoot>
+  );
+}
+
+export function Multiple() {
+  return (
+    <SelectRoot
+      onSelectedChange={(v) => console.log(v)}
+      multiple
+      placeholder="choose multiple option"
+    >
+      <PopupRoot>
+        <PopupTrigger asChild>
+          <SelectTrigger />
+        </PopupTrigger>
+        <PopupContent keepMounted>
+          <SelectOption value="1">one</SelectOption>
+          <SelectOption value="2">two</SelectOption>
+          <SelectOption value="3">three</SelectOption>
+          <SelectOption value="4">four</SelectOption>
+          <SelectOption value="5">five</SelectOption>
+        </PopupContent>
+      </PopupRoot>
+    </SelectRoot>
   );
 }
--- lib/Select/index.tsx
+++ lib/Select/index.tsx
@@ -5,187 +5,183 @@ import {
   useCompoundItem,
   useCompoundParent,
 } from "../useCompound";
-import { Popup } from "../Popup";
 import { useComposeRefs } from "../useComposeRefs";
-export default { title: "Components/Select" };
-
-interface UseSelectParams {
-  multiple: boolean;
-}
-
-// useSelect
-function useSelect(params: UseSelectParams) {
-  const { multiple } = params;
-  const { subitems: options, contextValue } = useCompoundParent();
-
-  const defaultValue = multiple ? [] : undefined;
-  const [selectedValue, setSelectedValue] = React.useState(defaultValue);
-  const [isOpen, setIsOpen] = React.useState(false);
-  const handleSelectedChange = React.useCallback(
-    (value) => {
-      if (multiple) {
-        if (selectedValue.includes(value)) {
-          setSelectedValue((prev) => prev.filter((v) => v !== value));
-        } else {
-          setSelectedValue((prev) => [...prev, value]);
-        }
-      } else {
-        setSelectedValue(value);
-        setIsOpen(false);
-      }
-    },
-    [multiple, selectedValue]
-  );
-
-  const getLabelByValue = React.useCallback(
-    (value) => {
-      const label = options.get(value)?.ref.current?.textContent ?? "";
-      return label;
-    },
-    [options]
-  );
-
-  React.useEffect(() => {
-    console.log(options, selectedValue);
-  }, [options, selectedValue]);
-
-  const toggleOpen = React.useCallback(() => {
-    console.log(1);
-    setIsOpen((prev) => !prev);
-  }, []);
-
-  const selectContextValue = React.useMemo(() => {
-    return {
-      options,
-      selectedValue,
-      handleSelectedChange,
-      getLabelByValue,
-      isOpen,
-      toggleOpen,
-    };
-  }, [
-    getLabelByValue,
-    handleSelectedChange,
-    isOpen,
-    options,
-    selectedValue,
-    toggleOpen,
-  ]);
-
-  return {
-    contextValue,
-    selectContextValue,
-  };
-}
+import { createContext } from "..";
 
 /**
- * context
- */
-interface SelectContextValue {}
-const SelectContext = React.createContext<SelectContextValue>({});
-
-function useSelectContext() {
-  const ctx = React.useContext(SelectContext);
-  if (!ctx) throw Error("must have SelectProvider");
-  return ctx;
-}
-
-/**
- * Provider
+ * SelectRoot
  */
 
-type SelectProviderValue = any;
-
-interface SelectProviderProps {
-  value: SelectProviderValue;
-  children: React.ReactNode;
-}
-function SelectProvider(props: SelectProviderProps) {
-  const { value: valueProps, children } = props;
-
-  const { contextValue, selectContextValue } = valueProps;
-
-  return (
-    <SelectContext.Provider value={selectContextValue}>
-      <CompoundComponentContext.Provider value={contextValue}>
-        {children}
-      </CompoundComponentContext.Provider>
-    </SelectContext.Provider>
-  );
+interface SelectRootContextValue {
+  selectedValue: string | string[];
+  handleSelectedValueChange: (value: string) => void;
+  getLabelByValue: (value: string) => string;
+  placeholder?: string;
 }
 
-interface SelectProps extends React.ComponentPropsWithoutRef<"div"> {
+const [SelectRootProvider, useSelectRoot] =
+  createContext<SelectRootContextValue>("SelectRoot");
+
+interface SelectRootProps extends React.ComponentPropsWithoutRef<"div"> {
   multiple?: boolean;
+  onSelectedChange?: (selected: string | string[]) => void;
+  defaultSelected?: string | string[];
+  placeholder?: string;
 }
-const Select = React.forwardRef<HTMLDivElement, SelectProps>(function Select(
-  props: SelectProps,
-  forwardRef
-) {
-  const { children, multiple = false, ...rest } = props;
-  const value = useSelect({ multiple });
 
-  const { getLabelByValue, selectedValue, isOpen, toggleOpen } =
-    value.selectContextValue;
+interface Metadata {
+  ref: React.RefObject<HTMLElement>;
+}
 
-  let label;
-  if (multiple && Array.isArray(selectedValue)) {
-    label = selectedValue.map((v) => getLabelByValue(v)).join(",");
-  } else {
-    label = getLabelByValue(selectedValue);
-  }
+const SelectRoot = React.forwardRef<HTMLDivElement, SelectRootProps>(
+  function SelectRoot(props: SelectRootProps, forwardRef) {
+    const {
+      children,
+      multiple = false,
+      onSelectedChange,
+      defaultSelected,
+      placeholder,
+      ...rest
+    } = props;
+    const defaultValue = defaultSelected ?? multiple ? [] : "";
+    const [selectedValue, setSelectedValue] = React.useState<string | string[]>(
+      defaultValue
+    );
+    const { subitems: options, contextValue } = useCompoundParent<
+      string,
+      Metadata
+    >();
 
-  const triggerRef = React.useRef(null);
+    React.useEffect(() => {
+      console.log(options, selectedValue);
+    }, [options, selectedValue]);
 
-  return (
-    <div ref={forwardRef} {...rest}>
-      <button onClick={toggleOpen} ref={triggerRef}>
-        selected {label}
-      </button>
-      <SelectProvider value={value}>
-        <Popup
-          keepMounted
-          open={isOpen}
-          anchor={triggerRef.current}
-          className="p-2 shadow max-w-sm border rounded"
+    const handleSelectedValueChange = React.useCallback(
+      (value: string) => {
+        if (multiple && Array.isArray(selectedValue)) {
+          if (selectedValue.includes(value)) {
+            setSelectedValue((prev) => {
+              if (Array.isArray(prev)) {
+                const next = prev.filter((v) => v !== value);
+                onSelectedChange?.(next);
+                return next;
+              } else {
+                return prev;
+              }
+            });
+          } else {
+            setSelectedValue((prev) => {
+              const next = [...prev, value];
+              onSelectedChange?.(next);
+              return next;
+            });
+          }
+        } else {
+          setSelectedValue(value);
+          onSelectedChange?.(value);
+        }
+      },
+      [multiple, onSelectedChange, selectedValue]
+    );
+
+    const getLabelByValue = React.useCallback(
+      (value: string) => {
+        const label = options.get(value)?.ref.current?.textContent ?? "";
+        return label;
+      },
+      [options]
+    );
+
+    return (
+      <CompoundComponentContext.Provider value={contextValue}>
+        <SelectRootProvider
+          selectedValue={selectedValue}
+          handleSelectedValueChange={handleSelectedValueChange}
+          getLabelByValue={getLabelByValue}
+          placeholder={placeholder}
         >
-          {children}
-        </Popup>
-      </SelectProvider>
-    </div>
-  );
-});
+          <div ref={forwardRef} {...rest}>
+            {children}
+          </div>
+        </SelectRootProvider>
+      </CompoundComponentContext.Provider>
+    );
+  }
+);
 
-interface OptionProps extends React.ComponentPropsWithoutRef<"div"> {
+/**
+ * SelectTrigger
+ */
+
+interface SelectTriggerProps extends React.ComponentPropsWithoutRef<"button"> {}
+const SelectTrigger = React.forwardRef<HTMLButtonElement, SelectTriggerProps>(
+  function SelectTrigger(props: SelectTriggerProps, forwardRef) {
+    const { children, ...rest } = props;
+    const { selectedValue, getLabelByValue, placeholder } =
+      useSelectRoot("SelectTriggerProps");
+
+    function showPlaceholder() {
+      if (Array.isArray(selectedValue)) {
+        return selectedValue.length === 0;
+      } else {
+        return !selectedValue;
+      }
+    }
+
+    function renderLabel() {
+      let label: string;
+      if (Array.isArray(selectedValue)) {
+        label = selectedValue.map((v) => getLabelByValue(v)).join(", ");
+      } else {
+        label = getLabelByValue(selectedValue);
+      }
+      return label;
+    }
+
+    return (
+      <button ref={forwardRef} {...rest}>
+        {showPlaceholder() && placeholder}
+        {renderLabel()}
+        {children}
+      </button>
+    );
+  }
+);
+
+/**
+ * SelectOption
+ */
+
+interface SelectOptionProps extends React.ComponentPropsWithoutRef<"button"> {
   value: string;
 }
+const SelectOption = React.forwardRef<HTMLButtonElement, SelectOptionProps>(
+  function SelectOption(props: SelectOptionProps, forwardRef) {
+    const { children, value, ...rest } = props;
+    const optionRef = React.useRef<HTMLButtonElement>(null);
+    const composedRef = useComposeRefs(optionRef, forwardRef);
+    const metadata = React.useMemo(() => {
+      return {
+        ref: optionRef,
+      };
+    }, []);
+    const { id: registedValue } = useCompoundItem(value, metadata);
+    const { handleSelectedValueChange, selectedValue } =
+      useSelectRoot("SelectOption");
+    const isSelected = Array.isArray(selectedValue)
+      ? selectedValue.includes(registedValue)
+      : registedValue === selectedValue;
+    return (
+      <button
+        data-selected={isSelected ? "true" : "false"}
+        ref={composedRef}
+        {...rest}
+        onClick={() => handleSelectedValueChange(registedValue)}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
-const Option = React.forwardRef<HTMLSpanElement, OptionProps>(function Option(
-  props: OptionProps,
-  forwardRef
-) {
-  const { children, value, ...rest } = props;
-
-  const ref = React.useRef<HTMLSpanElement>(null);
-  const composedRef = useComposeRefs(ref, forwardRef);
-  const metadata = React.useMemo(() => {
-    return {
-      value,
-      ref: ref,
-    };
-  }, [value]);
-  const { id: returnedId } = useCompoundItem(value, metadata);
-  const { handleSelectedChange } = useSelectContext();
-  return (
-    <span
-      ref={composedRef}
-      {...rest}
-      onClick={() => {
-        handleSelectedChange(returnedId);
-      }}
-    >
-      {children}
-    </span>
-  );
-});
-
-export { Select, Option };
+export { SelectRoot, SelectOption, SelectTrigger };
