29590d6348aae90442cf194b037bb06d72c8666a
chenyuhao <cccchenyuhao@gmail.com>
2023-12-02 14:50:35 +0800

    form message

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,14 +1,47 @@
-import React, { useId } from "react";
-import { createContext } from "../lib";
+import * as React from "react";
+import { createContext, useComposeRefs } from "../lib";
+
+// bultin
+const validityMatchers = [
+  "badInput",
+  "patternMismatch",
+  "rangeOverflow",
+  "rangeUnderflow",
+  "stepMismatch",
+  "tooLong",
+  "tooShort",
+  "typeMismatch",
+  "valid",
+  "valueMissing",
+] as const;
+type ValidityMatcher = (typeof validityMatchers)[number];
+const DEFAULT_INVALID_MESSAGE = "This value is not valid";
+const DEFAULT_BUILT_IN_MESSAGES: Record<ValidityMatcher, string | undefined> = {
+  badInput: DEFAULT_INVALID_MESSAGE,
+  patternMismatch: "This value does not match the required pattern",
+  rangeOverflow: "This value is too large",
+  rangeUnderflow: "This value is too small",
+  stepMismatch: "This value does not match the required step",
+  tooLong: "This value is too long",
+  tooShort: "This value is too short",
+  typeMismatch: "This value does not match the required type",
+  valid: undefined,
+  valueMissing: "This value is missing",
+};
 
 function FormDemo() {
   return (
     <div>
-      <FormRoot>
+      <FormRoot
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <FormField name="username">
           <FormLabel>username</FormLabel>
           <FormControl />
         </FormField>
+        <button type="submit">submit</button>
       </FormRoot>
     </div>
   );
@@ -40,7 +73,7 @@ interface FormFieldProps extends React.ComponentPropsWithoutRef<"div"> {
 
 const FormField = React.forwardRef<HTMLDivElement, FormFieldProps>(
   function FormField(props: FormFieldProps, forwardRef) {
-    const id = useId();
+    const id = React.useId();
     const { children, name, ...rest } = props;
     return (
       <FormFieldProvider name={name} id={id}>
@@ -72,7 +105,66 @@ const FormControl = React.forwardRef<HTMLInputElement, FormControlProps>(
     const { id: idProp, ...rest } = props;
     const { id: fieldId } = useFormField("FormControl");
     const id = idProp ?? fieldId;
-    return <input ref={forwardRef} id={id} {...rest} />;
+
+    const controlRef = React.useRef<HTMLInputElement>();
+
+    const composedRef = useComposeRefs(controlRef, forwardRef);
+
+    const updateControlValidity = React.useCallback(
+      function updateControlValidity(control: HTMLInputElement) {
+        console.log(control.validity);
+      },
+      []
+    );
+
+    React.useEffect(() => {
+      const control = controlRef.current;
+      if (control) {
+        const handleChange = () => {
+          updateControlValidity(control);
+        };
+        window.addEventListener("change", handleChange);
+        return () => window.removeEventListener("change", handleChange);
+      }
+    }, [updateControlValidity]);
+
+    return <input ref={composedRef} id={id} {...rest} />;
+  }
+);
+
+interface FormMessageProps extends React.ComponentPropsWithoutRef<"span"> {
+  match?: () => boolean | string;
+}
+
+const FormMessage = React.forwardRef<HTMLDivElement, FormMessageProps>(
+  function FormMessage(props: FormMessageProps, forwardRef) {
+    const { children, match, ...rest } = props;
+
+    if (match === undefined) {
+      return (
+        <FormMessageImp>{children ?? DEFAULT_INVALID_MESSAGE}</FormMessageImp>
+      );
+    } else if (typeof match === "function") {
+      return (
+        <FormMessageImp>{children ?? DEFAULT_INVALID_MESSAGE}</FormMessageImp>
+      );
+    } else {
+      <FormMessageImp>{children ?? DEFAULT_INVALID_MESSAGE}</FormMessageImp>;
+    }
+
+    return (
+      <span ref={forwardRef} {...rest}>
+        {children}
+      </span>
+    );
+  }
+);
+
+interface FormMessageImpProps extends React.ComponentPropsWithoutRef<"span"> {}
+const FormMessageImp = React.forwardRef<HTMLSpanElement, FormMessageImpProps>(
+  function FormMessageImp(props: FormMessageImpProps, forwardRef) {
+    const { ...rest } = props;
+    return <span ref={forwardRef} {...rest} />;
   }
 );
 
