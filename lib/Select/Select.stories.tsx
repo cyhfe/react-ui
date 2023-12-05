e3182d0cd9a3ba64c2399d42d28c25c3bdeef533
chenyuhao <cccchenyuhao@gmail.com>
2023-12-05 21:41:45 +0800

    update select story

--- lib/Select/Select.stories.tsx
+++ lib/Select/Select.stories.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   SelectLabel,
   SelectOption,
@@ -55,6 +56,44 @@ export function Single() {
 export function Multiple() {
   return (
     <SelectRoot placeholder="choose color" multiple>
+      <SelectTrigger className="w-80 min-h-[30px] px-3 py-2 text-left border rounded shadow">
+        <SelectPlaceholder className="text-slate-500" />
+        <SelectLabel />
+      </SelectTrigger>
+
+      <SelectPopup
+        keepMounted
+        className="flex flex-col gap-y-1 p-2 w-80 min-h-[30px] max-h-[200px] overflow-auto border rounded shadow "
+      >
+        {options.map((option) => {
+          return (
+            <SelectOption
+              key={option.value}
+              value={option.value}
+              className="text-left hover:bg-slate-100 px-3 py-2 data-[selected=true]:bg-blue-100 data-[selected=true]:text-blue-900 rounded  "
+            >
+              {option.label}
+            </SelectOption>
+          );
+        })}
+      </SelectPopup>
+    </SelectRoot>
+  );
+}
+
+export function Controlled() {
+  const [selected, setSelected] = useState("green");
+
+  useEffect(() => {
+    console.log(selected);
+  }, [selected]);
+
+  return (
+    <SelectRoot
+      placeholder="choose color"
+      selectedValue={selected}
+      onSelectedChange={(v) => setSelected(v as string)}
+    >
       <SelectTrigger className="w-80 min-h-[30px] px-3 py-2 text-left border rounded shadow">
         <SelectPlaceholder className="text-slate-500" />
         <SelectLabel />
